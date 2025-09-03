a77859d81d3f6bf38396acdca15a72217e7a92f4
danula-ded <stupishin1597@yandex.ru>
2025-09-03 14:42:13 +0300

    deploy frontend to github pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  base: process.env.GH_PAGES_BASE || "/ariesto/",
+});
